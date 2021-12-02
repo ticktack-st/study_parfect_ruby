7774a1b273d977864b103789faaddfd6fba50885
ticktack-st <moglandwrapper@gmail.com>
2021-12-02 15:51:06 +0900

    'putsの仕様'

---
+++ part2/p.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Ruler
+  def initialize(length)
+    @length = length
+  end
+
+  def to_s
+    '=' * @length
+  end
+
+  def inspect
+    "Ruler(length: #{@length})"
+  end
+end
+
+ruler = Ruler.new(30)
+
+puts ruler
+p ruler
+pp ruler
